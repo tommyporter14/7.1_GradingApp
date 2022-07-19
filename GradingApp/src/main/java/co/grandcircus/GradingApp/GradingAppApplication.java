6dd70924b4cce59b4f4a5326723d547c56d5e820
tommy <tommyporter14@gmail.com>
2022-07-18 22:29:26 -0400

    first commit, not working

---
+++ GradingApp/src/main/java/co/grandcircus/GradingApp/GradingAppApplication.java
@@ -0,0 +1,13 @@
+package co.grandcircus.GradingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradingAppApplication.class, args);
+	}
+
+}
---
+++ GradingApp/src/test/java/co/grandcircus/GradingApp/GradingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package co.grandcircus.GradingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
